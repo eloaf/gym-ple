2a31aa18e5d36b2cd1f7ec27a6494c7d45d9a269
Eric Laufer <elaufer@elementai.com>
2017-10-02 16:33:01 -0400

    modifications to work with OpenAI baselines

--- gym_ple/ple_env.py
+++ gym_ple/ple_env.py
@@ -4,13 +4,22 @@ from gym import spaces
 from ple import PLE
 import numpy as np
 
+
+class DummyALE:
+    # Dummy object to allow the following call to work: env.unwrapped.game_state.lives()
+    def __init__(self, ple_game_object):
+        self.ple_game_object = ple_game_object
+
+    def lives(self):
+        return self.ple_game_object.game_state.lives()
+
+
 class PLEEnv(gym.Env):
     metadata = {'render.modes': ['human', 'rgb_array']}
 
     def __init__(self, game_name='FlappyBird', display_screen=True):
         # set headless mode
         os.environ['SDL_VIDEODRIVER'] = 'dummy'
-        
         # open up a game state to communicate with emulator
         import importlib
         game_module_name = ('ple.games.%s' % game_name).lower()
@@ -19,13 +28,27 @@ class PLEEnv(gym.Env):
         self.game_state = PLE(game, fps=30, display_screen=display_screen)
         self.game_state.init()
         self._action_set = self.game_state.getActionSet()
+        # added line for compatibility with OpenAI baselines (at least a2c
+        # right now)
+        self._action_set = (['NOOP'] if None in self._action_set else []) + ([x for x in self._action_set if x])
         self.action_space = spaces.Discrete(len(self._action_set))
         self.screen_width, self.screen_height = self.game_state.getScreenDims()
         self.observation_space = spaces.Box(low=0, high=255, shape=(self.screen_width, self.screen_height, 3))
         self.viewer = None
 
+        # following two lines added for compat. with OpenAI baselines
+        self.ale = DummyALE(self)
+        self.np_random = self.game_state.rng
+
+    def get_action_meanings(self):
+        # Added for compat with openai baselines
+        return self._action_set
 
     def _step(self, a):
+        # following three lines for compat with OpenAI baselines
+        action = self._action_set[a]
+        if action=='NOOP':
+            action = None
         reward = self.game_state.act(self._action_set[a])
         state = self._get_image()
         terminal = self.game_state.game_over()
